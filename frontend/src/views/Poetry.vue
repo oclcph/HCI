0b6671cf456805c8c0acacb65a8274d266563cb9
cph <221830224@smail.nju.edu.cn>
2024-12-23 17:19:32 +0800

    添加诗歌页面

---
+++ frontend/src/views/Poetry.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="level">
+    <h1 class="text-2xl font-bold">个人中心</h1>
+    <p>欢迎来到个人中心！</p>
+    <div>
+      <h2 class="text-xl">个人信息</h2>
+      <p>姓名: {{ }}</p>
+      <p>邮箱: {{  }}</p>
+      <!-- 其他个人信息 -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {onMounted} from "vue";
+import {getPoetry} from "../api/poetry";
+import { defineComponent, ref } from 'vue';
+
+interface Poetry {
+  id: number;
+  title: string;
+  author: string;
+  type: string;
+
+  level: string;
+  dynasty: string;
+}
+
+export default defineComponent({
+  name:'Level',
+  setup(){
+    const poetry = ref<Poetry[]>([]);
+    return {
+      poetry,
+    }
+  }
+});
+
+</script>
+
+<style scoped>
+
+</style>
